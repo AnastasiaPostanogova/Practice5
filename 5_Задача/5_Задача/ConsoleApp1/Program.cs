bee2a78635b09d402f2b26382cb01d4ef6724202
AnastasiaPostanogova <35628928+AnastasiaPostanogova@users.noreply.github.com>
2018-06-25 21:21:55 +0500

    full

---
+++ 5_Задача/5_Задача/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 8;
+        int m = 8;
+        int[,] mas = new int[n, m];
+        int tekn = 0, tekm = 0;
+        int znach = 1;
+        while (tekm < m)
+        {
+            do
+            {
+                mas[tekn, tekm] = znach;
+                znach++;
+                tekn++;
+            } while (tekn < n);
+            tekn--;
+            tekm++;
+            if (znach > n * m) break;
+            do
+            {
+                mas[tekn, tekm] = znach;
+                znach++;
+                tekn--;
+            } while (tekn >= 0);
+            tekn++;
+            tekm++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+                Console.Write("{0,4}", mas[i, j]);
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+} 
